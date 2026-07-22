4618a73bc7c86e39c0621ba11835f2a83b742da6
Abhishek Badiger <abhishek.b@medtrixhealthcare.com>
2026-07-22 16:26:14 +0530

    added kumar sir email

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -38,7 +38,7 @@ export default function TopNav() {
   const initials = userEmail ? userEmail[0].toUpperCase() : "U";
 
   async function handleMLRConnect() {
-    const email = 'shijin.p@medtrixhealthcare.com';
+    const email = 'kumar@medtrixhealthcare.com';
     const mlrUrl = encodeURI(`http://tuned.mlrcatalyst.com/MLRCatalyst/VerifyOTP?emailAddress=${email}`);
   
 
